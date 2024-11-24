a523ad059703b9bc0e6469bb648615004cc437d3
Carlos Eduardo Angeline <x29153049802@tcu.gov.br>
2024-11-24 14:46:47 -0300

    List user id

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -9,7 +9,8 @@ def _create_user():
     user = User(username=data['username'])
     db.session.add(user)
     db.session.commit()
-    
+
+#list users    
 def _list_users():
     query = db.select(User)
     users = db.session.execute(query).scalars()
@@ -21,6 +22,7 @@ def _list_users():
   for result in users
 	]
 
+#create user
 @app.route('/', methods=['GET', 'POST'])
 def handle_user():
     if request.method == 'POST':
@@ -28,5 +30,15 @@ def handle_user():
         return {'message': 'User Created!'}, HTTPStatus.CREATED
     else:
         return {'users': _list_users()} 
+
+#list user por id    
+@app.route('/<int:user_id>')
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return {
+		"id": user.id,
+        "username": user.username,
+	}
+    
     
     
